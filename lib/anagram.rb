517e2a54586b6dd36b7dc3c731d76fe3beabb348
Neil Empey <argus.two.2@gmail.com>
2020-04-23 21:54:33 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    solution = []
+     array.map do |ele|
+      if ele.length == @word.length
+            test1 = ele.split('').sort
+            test2 = @word.split('').sort
+            if (test1 == test2)
+              solution << ele
+            end
+        end
+      end
+       return solution
+    end
+end
